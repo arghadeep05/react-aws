44708648e235b27ca391f4e679216a725078c03e
arghadeep saha <arghadeep05@gmail.com>
2023-11-23 13:51:01 +0530

    quiz app

---
+++ src/main.tf
@@ -0,0 +1,40 @@
+provider "aws"{
+    region="ap-south-1"
+    shared_credentials_files = "~/.aws/credentials" 
+    profile="personal"
+}
+variable "bucket_name" {
+    default="s3-react-terraform"
+}
+resource "aws_s3_bucket" "react_bucket"{
+    bucket = "${var.bucket_name}"
+    acl="public-read"
+    policy=<<EOF
+    {
+        "Id":"bucket_policy_site",
+        "Version":"2012-10-17",
+        "Statement":[
+            {
+                "Sid":"bucket_policy_site_main",
+                "Action":["s3:getObject"],
+                "Effect":"Allow".
+                "Resource":"arn:aws:s3:::${var.bucket_name}/*",
+                "Principal":"*"
+            }
+        ]
+    }
+    EOF
+
+    website {
+        index_document = "index.html"
+        error_document = "index.html"
+    }
+}
+output "website_domain" {
+        value = "${aws_s3_bucket.react_bucket.website_domain}"
+ 
+}
+output "website_endpoint" {
+        value = "${aws_s3_bucket.react_bucket.website_endpoint}"
+ 
+}
